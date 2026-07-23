a60952cc914819dfccb5cb4b7b9a8b76bb85aa61
fikalaila <fikalailaturamadani@gmail.com>
2026-07-23 15:17:36 +0700

    Menambahkan link login

--- index.php
+++ index.php
@@ -1800,7 +1800,7 @@ date_default_timezone_set("Asia/Jakarta");
           </svg>
         </button>
         <a href="#contact" class="nav-cta" data-en="Contact Us">Hubungi Kami</a>
-        <a href="login.php" class="nav-cta" data-en="Login">Login</a>
+        <a href="admin_mbc/login.php" class="nav-cta" data-en="Login">Login</a>
       </div>
     </div>
   </header>
